bba94a89780b93f17695a8c5b4be15ee47ff4723
Mujahid Ahmed <mujahidahmed99@hotmail.co.uk>
2021-09-25 16:25:28 +0100

    BlogApp Creation

---
+++ BlogApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BlogApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BlogApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
